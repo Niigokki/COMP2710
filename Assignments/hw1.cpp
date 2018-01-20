791e10ea6e33ce52cd1d660ec8b984e97ffed577
MK <jacob.kaleen.smith@gmail.com>
2018-01-20 10:35:02 -0600

    vars setup, taking user input

--- Assignments/hw1.cpp
+++ Assignments/hw1.cpp
@@ -1,5 +1,6 @@
 //Jacob Smith
 //jks0039
+//just a simple program that takes input, and performs a few simple calculations
 //hw1.cpp
 //just use gcc/g++
 #include <iostream>
@@ -7,8 +8,15 @@ using namespace std;
 
 int main()
 {
+   int amountSweetner; 
    cout << "What is the amount of artificial sweetner needed to kill a mouse?" << endl;
+   cin >> amountSweetner;
    cout << "What is the weight of the mouse?" << endl;
+   int mouseWeight;
+   cin >> mouseWeight;
    cout << "What is the weight of the dieter?" << endl;
+   int dieterWeight;
+   cin >> dieterWeight;
+   return 0;
       
 }
