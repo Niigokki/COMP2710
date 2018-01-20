0f4a3a1219152e224358c7bef8f8c0c6e1b28ee7
MK <jacob.kaleen.smith@gmail.com>
2018-01-20 10:19:19 -0600

    started hw1, setting up print statements

---
+++ Assignments/hw1.cpp
@@ -0,0 +1,14 @@
+//Jacob Smith
+//jks0039
+//hw1.cpp
+//just use gcc/g++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "What is the amount of artificial sweetner needed to kill a mouse?" << endl;
+   cout << "What is the weight of the mouse?" << endl;
+   cout << "What is the weight of the dieter?" << endl;
+      
+}
