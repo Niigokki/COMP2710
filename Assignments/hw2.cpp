63f3fa28e219001c04a8e95fe44ce99e230ed96e
MK <jacob.kaleen.smith@gmail.com>
2018-02-07 07:30:56 -0600

    starting HW2, init vars

---
+++ Assignments/hw2.cpp
@@ -0,0 +1,17 @@
+/*
+ * Jacob Smith
+ * jks0039
+ * hw2.cpp
+ * Compiled using g++
+ * g++ -o hw2 hw2.cpp
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+
+double loan_amount, interest_rate, payments;
+
+
+
+}
