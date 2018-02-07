7bb42a3f2415a7821a164c5e0353aae3a230d57d
MK <jacob.kaleen.smith@gmail.com>
2018-02-07 08:29:38 -0600

    fixed formatting for now

--- Assignments/hw2.cpp
+++ Assignments/hw2.cpp
@@ -10,14 +10,15 @@ using namespace std;
 
 int main() {
 
-double amount_loan, interest_rate, monthly_payments;
+double loan_amount, interest_rate, monthly_payments, loan_balance, monthly_rate, loan_principal;
+int months_paid;
 //the magic formula for formatting
 cout.setf(ios::fixed);
 	cout.setf(ios::showpoint);
 	cout.precision(2);
 	
 	cout << "Loan Amount: ";
-	cin >> amount_loan;
+	cin >> loan_amount;
 	cout << "Interest Rate (% per year): ";
 	cin >> interest_rate;
 	cout << "Monthly Payments: ";
@@ -27,7 +28,19 @@ cout.setf(ios::fixed);
 	cout << "*********************************************************************";
 	cout << "\n\tAmortization Table";
 	cout << "\n*********************************************************************";
-
+	//before we do any further calcs, we want to initalize our loan_balance and monthly rates
+	loan_balance = loan_amount;
+	monthly_rate = (interest_rate/12);
+	double payment = (monthly_rate * loan_amount);
+	//and setup table headers
+	cout << "\nMonth\tBalance\t\tPayment\tRate\tInterest\tPrincipal";
+	cout << "\n" << months_paid << "\t" << loan_balance << "\t\t" << monthly_payments << "\t" << monthly_rate << "\t" << payment << "\t\t" << loan_amount;
+	/*while (loan_balance > 0) {
+		
+		months_paid++;
+		//interest = principal * rate * time
+		loan_balance - loan_balance - 500;
+	}*/
 
 
 }
