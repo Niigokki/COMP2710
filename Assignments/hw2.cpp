5460f2abfc6e206b0d4fa0def5d30463bd8b4663
MK <jacob.kaleen.smith@gmail.com>
2018-02-07 22:11:29 -0600

    assignment finished: formatting was annoying

--- Assignments/hw2.cpp
+++ Assignments/hw2.cpp
@@ -6,41 +6,62 @@
  * g++ -o hw2 hw2.cpp
  */
 #include <iostream>
+
 using namespace std;
 
 int main() {
 
-double loan_amount, interest_rate, monthly_payments, loan_balance, monthly_rate, loan_principal;
-int months_paid;
-//the magic formula for formatting
-cout.setf(ios::fixed);
-	cout.setf(ios::showpoint);
-	cout.precision(2);
-	
-	cout << "Loan Amount: ";
-	cin >> loan_amount;
-	cout << "Interest Rate (% per year): ";
-	cin >> interest_rate;
-	cout << "Monthly Payments: ";
-	cin >> monthly_payments;
+    double total_loan, interest_paid, interest, interest_rate, monthly_cost, loan_balance, monthly_rate, loan_principal;
+    int months_paid = 0;
+    //the magic formula for formatting
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+
+    cout << "Loan Amount: ";
+    cin >> total_loan;
+    cout << "Interest Rate (% per year): ";
+    cin >> interest_rate;
+    cout << "Monthly Payments: ";
+    cin >> monthly_cost;
 
 
-	cout << "*********************************************************************";
-	cout << "\n\tAmortization Table";
-	cout << "\n*********************************************************************";
-	//before we do any further calcs, we want to initalize our loan_balance and monthly rates
-	loan_balance = loan_amount;
-	monthly_rate = (interest_rate/12);
-	double payment = (monthly_rate * loan_amount);
-	//and setup table headers
-	cout << "\nMonth\tBalance\t\tPayment\tRate\tInterest\tPrincipal";
-	cout << "\n" << months_paid << "\t" << loan_balance << "\t\t" << monthly_payments << "\t" << monthly_rate << "\t" << payment << "\t\t" << loan_amount;
-	/*while (loan_balance > 0) {
-		
-		months_paid++;
-		//interest = principal * rate * time
-		loan_balance - loan_balance - 500;
-	}*/
+    cout << "*********************************************************************" << "\n\tAmortization Table" << "\n*********************************************************************";
+    //before we do any further calcs, we want to initalize our loan_balance and monthly rates
+    loan_balance = total_loan;
+    monthly_rate = (interest_rate / 12);
+    //and setup table headers
+    cout << "\nMonth\tBalance\t\tPayment\tRate\tInterest\tPrincipal";
+    //formatting sucks this way : TODO find better way to handle this
+    cout << "\n" << months_paid << "\t$" << loan_balance << "\tN/A\tN/A\tN/A\t\tN/A";
+
+    if (monthly_cost <= ((interest_rate / 12) * total_loan) / 100) {
+        cout << "\nRegular payments must be larger than monthly interest\n";
+        return 0;
+    }
+    while (loan_balance > 0) {
+        //calculate interest for the month,divided by 100 to get a decimal
+        interest = ((loan_balance * monthly_rate) / 100);
+        //calculate principal removed after interest
+        loan_principal = monthly_cost - interest;
+        //calculate balance after removing these things
+        loan_balance = loan_balance - loan_principal;
+        interest_paid += interest;
+        ++months_paid;
+
+        if (loan_balance <= 0) {
+            loan_principal = loan_balance + loan_principal;
+            monthly_cost = loan_principal + interest;
+            loan_balance = 0;
+        }
+
+        cout << "\n" << months_paid << "\t$" << loan_balance << "\t\t$" << monthly_cost << "\t$" << interest_rate
+             << "\t$" << interest << "\t\t$" << loan_principal;
+    }
+
+    cout << "\n*********************************************************************" << "\n\nIt takes " << months_paid
+         << " months to pay off the loan." << "\nTotal interest paid is: $" << interest_paid << "\n";
+    return 0;
 
 
 }
