70934948912ad9eff9dc071103db8c0dba8f2a35
Jacob Smith <jacob.kaleen.smith@gmail.com>
2018-03-15 10:30:11 -0500

    added list initalizer

--- Assignments/hw5.cpp
+++ Assignments/hw5.cpp
@@ -11,13 +11,28 @@
 #include <string>
 
 using namespace std;
-
+//our data structure
 struct TriviaNode{
 	string question;
 	string answer;
 	int point_value;
 	TriviaNode *next;
 };
+//list initalizer
+class TriviaList{
+	int count = 0;
+	TriviaNode *current, *original;
+	TriviaList() {
+		current = NULL;
+		original = NULL;
+	}
+};
+//init a list with our hardcoded questions
+void initList(TriviaList &someList) {
+	TriviaNode *q1 = new TriviaNode;
+}
+
+
 
 int main() {
 return 0;
