44376ce9852e0d9801a0b403072de0a85b99786b
Jacob Smith <jacob.kaleen.smith@gmail.com>
2018-03-15 10:24:18 -0500

    created TriviaNode struct

---
+++ Assignments/hw5.cpp
@@ -0,0 +1,24 @@
+ /*
+  * Jacob Smith
+  * jks0039
+  * hw5.cpp
+  * as always, method stubs taken from PDF.
+  * Compiled using g++
+  * g++ -o hw5 hw5.cpp
+  */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct TriviaNode{
+	string question;
+	string answer;
+	int point_value;
+	TriviaNode *next;
+};
+
+int main() {
+return 0;
+}
