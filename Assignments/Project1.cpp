b38fad3dbef0723e99299c435c8767a889379c24
Jacob Smith <jsmith@myocv.com>
2018-04-01 14:14:05 -0400

    init player struct

---
+++ Assignments/Project1.cpp
@@ -0,0 +1,26 @@
+/*
+  * Jacob Smith
+  * jks0039
+  * Project1.cpp
+  * as always, method stubs taken from PDF.
+  * Compiled using g++
+  * g++ -o Project1 Project1.cpp
+  */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Player{
+	int overall_score;
+	int intellegence;
+	int time;
+	int money;
+	string name;
+	bool is_alive;
+};
+//dummy code for now
+int main() {
+	return 0;
+}
+
